78479fdf180f4b1e6c41125a423ce2d38157c340
osoyliz <lizosoy18@gmail.com>
2018-07-16 20:22:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,10 @@
-
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+ninety_nine_problems.name
+  # => "99 Problems"
+ 
+ninety_nine_problems.artist
+  # => "Jay-Z"
+ 
+ninety_nine_problems.genre
+  # => "rap"
+end
